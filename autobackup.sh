735ccdbdf0b66a517facaa23911af31931436244
Aron0x0 <209122750+Aron0x0@users.noreply.github.com>
2025-12-25 14:46:57 +0000

    Version 0.1

---
+++ autobackup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+echo "Backup Script Started"
+
+ROOT_DIR="$HOME/minecraft"
+BACKUP_DIR="/root/minecraft/WorldsBackup"
+WORLD_NAME="Bedrock level"
+
+
+mkdir -p "$BACKUP_DIR"
+
+./command.sh say §6Running World Backup...
+sleep 2
+./command.sh say §4Expect a laggging...
+./command.sh save hold
+
+sleep 15
+
+tar --warning=no-file-changed -czf "$BACKUP_DIR/bedrock_world_$(date +%F_%H-%M-%S).tar.gz" \
+  -C "$ROOT_DIR/worlds" "$WORLD_NAME"
+
+./command.sh save resume
+./command.sh say §aBackup Completed Sucessfully!
+
+echo "Backup Finished Safely."
